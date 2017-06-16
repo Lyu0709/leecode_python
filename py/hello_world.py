d842e460b36741c27977dcf23091d65f9af282da
Jane Liu <jane.liu@movit-tech.com>
2017-06-16 17:25:32 +0800

    py

---
+++ py/hello_world.py
@@ -0,0 +1,9 @@
+PI = 3.14158
+def f(x):
+    return x + 2
+
+def g(a, b):
+    return a + b
+
+def hello():
+    print("hello world")
---
+++ py/learningPython.py
@@ -0,0 +1,7 @@
+import hello_world
+
+result = hello_world.f(5)
+add = hello_world.g(3, 6)
+print(result)
+print(add)
+print(hello_world.PI)
