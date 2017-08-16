28e6b2ff02ffd41fc72e805791b62745066be33d
Liu yu <jane.liu@movit-tech.com>
2017-08-16 16:43:50 +0800

    deletePython

--- py/hello_world.py
+++
@@ -1,9 +0,0 @@
-PI = 3.14158
-def f(x):
-    return x + 2
-
-def g(a, b):
-    return a + b
-
-def hello():
-    print("hello world")
--- py/learningPython.py
+++
@@ -1,7 +0,0 @@
-import hello_world
-
-result = hello_world.f(5)
-add = hello_world.g(3, 6)
-print(result)
-print(add)
-print(hello_world.PI)
